346f0ff61a4279afb8d7b2a44728b9b9ccac288b
André Luferat <contato@luferat.net>
2023-07-06 21:28:34 -0300

    Bug do contato corrigido

--- src/app/pages/contacts/contacts-routing.module.ts
+++ src/app/pages/contacts/contacts-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { ContactsPage } from './contacts.page';
 
 const routes: Routes = [
--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-contacts',
@@ -7,9 +10,113 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsPage implements OnInit {
 
-  constructor() { }
+  env = environment;
+  contactForm!: FormGroup;
+  contact!: any;
+  success: boolean = false;
+  firstName: String = "Visitante";
 
-  ngOnInit() {
+  validationMessages: any = {
+    name: {
+      required: 'O nome é obrigatório.',
+      minlength: 'O nome está muito curto.'
+    },
+    email: {
+      required: 'O email é obrigatório.',
+      email: 'Digite um email válido.'
+    },
+    subject: {
+      required: 'O assunto é obrigatório.'
+    },
+    message: {
+      required: 'A mensagem é obrigatória.'
+    }
+  }
+
+  formErrors: any = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+    this.success = false;
+
+    this.contactForm.valueChanges.subscribe(() => {
+      this.updateValidationMessages();
+    });
+  }
+
+  createForm() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  updateValidationMessages() {
+    for (const field in this.formErrors) {
+      if (Object.prototype.hasOwnProperty.call(this.formErrors, field)) {
+
+        this.formErrors[field] = '';
+
+        const control = this.contactForm.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (Object.prototype.hasOwnProperty.call(control.errors, key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  sendContact() {
+
+    if (this.contactForm.invalid) {
+      return;
+    }
+
+    this.contact = this.contactForm.value;
+    this.contact.date = new Date();
+    this.contact.status = 'sended';
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    this.http.post(environment.apiURL + '/contacts', this.contact, httpOptions)
+      .subscribe(
+        (data) => {
+          this.firstName = this.contact.name.split(' ')[0];
+          this.success = true;
+        },
+        (error) => {
+          alert('Oooops!\n' + error.message);
+        }
+      );
+
+    this.contactForm.reset();
+
+  }
+
+  reset() {
+    this.success = false;
+    return false;
   }
 
 }
